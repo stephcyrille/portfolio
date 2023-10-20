4db7d5d799743fef21c7ec273cd0479763d88111
Stephcyrille <stephcyril.sc@gmail.com>
2023-10-20 11:37:41 +0200

    Adding postcss in the projet, the header menu and Remove bootstrap to avoid conflicts with talwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import { Header } from './component/Header';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
