e0e66a573069a06cc1673934be341aba0421b9c5
Stephcyrille <stephcyril.sc@gmail.com>
2024-02-06 06:44:04 +0100

    Update star wars component

--- src/component/experience/ExperienceStarWars.jsx
+++ src/component/experience/ExperienceStarWars.jsx
@@ -1,4 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import bpop from '../../assets/images/logo_bp_aura.jpeg'
+import basf from '../../assets/images/basf.svg'
+import ewonga from '../../assets/images/logoEwonga.png'
+import eneo from '../../assets/images/eneo.png'
 
 export const ExperienceStarWarComponent = () => {
 
@@ -137,43 +141,57 @@ export const ExperienceStarWarComponent = () => {
               <div className="card-container-infos">
                   <div className="infos-title">
                       <div className="logos">
-                          <img src="https://star-wars-lime.vercel.app/assets/logo-1.png" className="logo-1 logo active" alt="logo-1" />
-                          <img src="https://star-wars-lime.vercel.app/assets/logo-2.png" className="logo-2 logo " alt="logo-2" />
-                          <img src="https://star-wars-lime.vercel.app/assets/logo-3.png" className="logo-3 logo " alt="logo-3" />
+                          <img src={bpop} className="logo-1 logo active rounded-full" alt="logo-1" />
+                          <img src={basf} className="logo-2 logo rounded-full" alt="logo-2" />
+                          <img src={eneo} className="logo-3 logo rounded-full" alt="logo-3" />
+                          <img src={ewonga} className="logo-4 logo rounded-full" alt="logo-4" />
                       </div>
                       <div className="titles">
-                          <h2 className="title-1 title active">Un nouvel espoir</h2>
-                          <h2 className="title-2 title ">L'empire contre-attaque</h2>
-                          <h2 className="title-3 title ">Le retour du Jedi</h2>
+                          <h2 className="title-1 title active">[Alternace] Datascientist</h2>
+                          <h2 className="title-2 title ">[Stage Alterné] Data Analyst</h2>
+                          <h2 className="title-3 title ">[CDD] Data Analyst</h2>
+                          <h2 className="title-4 title ">[Consultant] Développeur Fullstact</h2>
                       </div>
                       <div className="descriptions">
                           <p className="description-1 description active">
-                              C'est le premier opus de la saga Star Wars par sa date de sortie, mais le quatrième selon
-                              l'ordre chronologique de l'histoire.
+                            <span>Banque Populaire AURA (en cours)</span><br />
+                            <span>Corenc, France</span><br />
+                            <span>De septembre 2023 - Septembre 2024 [12 mois]</span><br />
                           </p>
                           <p className="description-2 description ">
-                              C'est le premier opus de la saga Star Wars par sa date de sortie, mais le quatrième selon l'ordre chronologique de l'histoire.
+                            <span>BASF Division Agronomique</span><br />
+                            <span>Ecully, France</span><br />
+                            <span>Avril 2023 - Aout 2023 [5 mois]</span><br />
                           </p>
                           <p className="description-3 description ">
-                              Le maléfique Empire galactique construit une nouvelle station spatiale Étoile de la mort pour anéantir définitivement l'Alliance rebelle.
+                            <span>ENEO</span><br />
+                            <span>Douala, Cameroun</span><br />
+                            <span>Mars 2022 - Aout 2022 [6 mois]</span><br />
+                          </p>
+                          <p className="description-4 description ">
+                            <span>Ewonga Sarl</span><br />
+                            <span>Douala, Cameroun</span><br />
+                            <span>janvier 2018 - Janvier 2023 [6 ans]</span><br />
                           </p>
                       </div>
                   </div>
                   <div className="infos-year">
                       <h1>
-                          <span>19</span>
+                          <span>20</span>
                           <span className="numbers">
-                              <span className="number-1 number active">77</span>
-                              <span className="number-2 number">80</span>
-                              <span className="number-3 number">83</span>
+                              <span className="number-1 number active">24</span>
+                              <span className="number-2 number">23</span>
+                              <span className="number-3 number">22</span>
+                              <span className="number-4 number">18</span>
                           </span>
                       </h1>
                   </div>
               </div>
               <div className="card-container-img">
-                  <img src="https://star-wars-lime.vercel.app/assets/bg-1.png" className="bg-1 img-card active" alt="bg-1" />
-                  <img src="https://star-wars-lime.vercel.app/assets/bg-2.png" className="bg-2 img-card" alt="bg-2" />
-                  <img src="https://star-wars-lime.vercel.app/assets/bg-3.png" className="bg-3 img-card" alt="bg-3" />
+                  <img src="https://images.unsplash.com/photo-1565347878219-552c839f1447?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="bg-1 img-card active" alt="bg-1" />
+                  <img src="https://plus.unsplash.com/premium_photo-1678988086482-2665e9394d73?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="bg-2 img-card" alt="bg-2" />
+                  <img src="https://images.unsplash.com/uploads/14114004461850596b57a/4855d5d2?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="bg-3 img-card" alt="bg-3" />
+                  <img src="https://images.unsplash.com/photo-1547658719-da2b51169166?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="bg-4 img-card" alt="bg-4" />
               </div>
           </div>
           <div className="card-evolution">
