05ee3f3154a63e32c7c6ffcab81e1d967c44856b
Stephcyrille <stephcyril.sc@gmail.com>
2024-02-11 22:43:43 +0100

    adding language selection on mobile

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -263,6 +263,60 @@ export const Header = () => {
                 >
                   Se connecter
                 </a> */}
+                <Popover className="relative">
+                  <Popover.Button 
+                    className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 focus-visible:outline-none 
+                              hover:bg-lime-100 py-2 px-3 rounded-lg ease-in-out delay-80 duration-200">
+                    
+                      <LanguageIcon className='mr-2 h-6 w-6' style={{fontSize: 26}} /> {t('french')}
+                      <img src={french} alt="French flag" className='ml-2 w-4 h-4' />
+                    <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                  </Popover.Button>
+                  <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute top-full z-10 mt-3 w-56 max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-lime-800">
+                        <div className="py-4">
+                          <button
+                            className="w-full"
+                            type='button'
+                          >
+                            <div 
+                              className={`flex py-2 pl-1 ${isFrench ? 'bg-lime-200 hover:bg-lime-200' : 'hover:bg-gray-100'}`}
+                              onClick={() =>{ 
+                                i18n.changeLanguage('fr')
+                                setIsFrench(true)
+                                setIsEnglish(false)
+                              }}
+                            >
+                              <img src={french} alt="French flag" className='mr-4 w-6 h-6 ml-3' /> <span>{t('french')}</span> 
+                            </div>
+                          </button>
+                          <button
+                            className="w-full"
+                            type='button'
+                          >
+                            <div 
+                              className={`flex py-2 pl-1 ${isEnglish ? 'bg-lime-200 hover:bg-lime-200' : 'hover:bg-gray-100'}`}
+                              onClick={() =>{ 
+                                i18n.changeLanguage('en')
+                                setIsEnglish(true)
+                                setIsFrench(false)
+                              }}
+                            >
+                              <img src={english} alt="French flag" className='mr-4 w-6 h-6 ml-3' /> <span>{t('english')}</span> 
+                            </div>
+                          </button>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>  
+                  </Popover>
               </div>
             </div>
           </div>
