d22e5c3c0dec63df84a86a54fe9cd6d3e8c4ffa6
Stephcyrille <stephcyril.sc@gmail.com>
2024-03-07 14:34:04 +0100

    Scroll the image list card when we put the cursor in the corne

--- src/pages/f1-predictor/components/DriverCharactersList.jsx
+++ src/pages/f1-predictor/components/DriverCharactersList.jsx
@@ -1,18 +1,58 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useRef } from "react";
 
-export const DriverCharactersList = forwardRef(({drivers, driver_index, handleImageClick}, ref) => {
+export const DriverCharactersList = forwardRef(({ drivers, driver_index, handleImageClick }, ref) => {
+  const divRef = useRef(null);
 
-  return <div
-    ref={ref}
-    className='flex items-center text-slate-400 dark:text-slate-50 overflow-x-auto bg-slate-500 py-8 my-16 lg:my-16'
-  >
-    {drivers.map((driver, index) => (
-      <img
-        key={driver.id}
-        src={driver.image_src}
-        alt={driver.name}
-        className={`w-28 h-28 md:w-44 md:h-44 lg:w-44 lg:h-44 rounded-full mx-2 hover:border-lime-400 border-4 cursor-pointer hover:scale-125 ease-out duration-300 ${driver_index === index && 'scale-125 border-lime-400 mx-6'}`}
-        onMouseOver={() => handleImageClick(index)} />
-    ))}
-  </div>;
-})
+  const handleMouseMove = (event) => {
+    const rect = divRef.current.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+
+    // Calculate the width of the corner area (e.g., 50 pixels)
+    const cornerWidth = 50;
+
+    // Check if the mouse is in the left or right corner
+    const isLeftCorner = x <= cornerWidth;
+    const isRightCorner = x >= rect.width - cornerWidth;
+
+    if (isLeftCorner) {
+      console.log("Mouse is in the left corner", ref);
+      ref.current.scrollTo({
+        top: 0,
+        left: -300,
+        behavior: "smooth",
+      });
+    } else if (isRightCorner) {
+      console.log("Mouse is in the right corner");
+      ref.current.scrollTo({
+        top: 0,
+        left: ref.current.scrollLeft + 300,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <>
+      <div
+        ref={(el) => {
+          divRef.current = el;
+          if (ref) {
+            ref.current = el;
+          }
+        }}
+        className='flex items-center text-slate-400 dark:text-slate-50 overflow-x-auto bg-slate-500 py-8 my-16 lg:my-16'
+        onMouseMove={handleMouseMove}
+      >
+        {drivers.map((driver, index) => (
+          <img
+            key={driver.id}
+            src={driver.image_src}
+            alt={driver.name}
+            className={`w-28 h-28 md:w-44 md:h-44 lg:w-44 lg:h-44 rounded-full mx-2 hover:border-lime-400 border-4 cursor-pointer hover:scale-125 ease-out duration-300 ${driver_index === index && 'scale-125 border-lime-400 mx-6'}`}
+            onMouseOver={() => handleImageClick(index)}
+          />
+        ))}
+      </div>
+    </>
+  );
+});
