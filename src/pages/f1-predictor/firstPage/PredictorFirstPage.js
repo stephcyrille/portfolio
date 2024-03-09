5f4e91558b1d9cba84d4e2a95704621a4ba2a303
Stephcyrille <stephcyril.sc@gmail.com>
2024-03-09 17:19:46 +0100

    Adding background in the driver selection page

--- src/pages/f1-predictor/firstPage/PredictorFirstPage.js
+++ src/pages/f1-predictor/firstPage/PredictorFirstPage.js
@@ -26,7 +26,7 @@ export const PredictorFirstPage = (props) => {
   const navigate = useNavigate();
 
   const containerRef = useRef(null);
-  const [selectedDriverIndex, setSelectedDriverIndex] = useState(0);
+  const [selectedDriverIndex, setSelectedDriverIndex] = useState(6);
 
   const handleImageHover = (index) => {
     setSelectedDriverIndex(index);
@@ -43,17 +43,24 @@ export const PredictorFirstPage = (props) => {
       initial="initial"
       animate="final"
     >
-
-      <div className='bg-gradient-to-r from-indigo-100 via-purple-100 to-teal-100 h-screen dark:from-lime-200 dark:via-gray-600 dark:to-gray-600 pt-32'>
-        <div className='text-gray-900 dark:text-slate-50 pb-0'>
-          <h1 className='text-center lg:text-xl md:text-xl'>
-            Select the driver
-          </h1>
-          <ItemNameDisplay data={drivers} item_index={selectedDriverIndex} />
+      <div className="relative mix-blend-overlay">
+        <img src={drivers[selectedDriverIndex].team} alt="BannerImage" className="absolute h-screen lg:h-screen w-full object-cover object-right z-10" />
+        <div className="absolute z-20 bg-gradient-to-r from-gray-900 via-gray-600 to-red-300 dark:from-gray-900 dark:via-lime-800 dark:to-gray-900 h-screen lg:h-screen w-full opacity-60" />
+        <div className="absolute z-20 h-screen lg:h-screen w-full pt-32">
+          <div className='text-amber-400 dark:text-lime-400 pb-0'>
+            <h1 className='text-center lg:text-xl md:text-xl text-white'>
+              Select the driver
+            </h1>
+            <ItemNameDisplay data={drivers} item_index={selectedDriverIndex} />
+          </div>
+          <DriverBigCard drivers={drivers} driver_index={selectedDriverIndex} />
+          <DriverCharactersList ref={containerRef} drivers={drivers} driver_index={selectedDriverIndex} handleImageHover={handleImageHover} handleImageClick={handleImageClick} />
         </div>
-        <DriverBigCard drivers={drivers} driver_index={selectedDriverIndex} />
-        <DriverCharactersList ref={containerRef} drivers={drivers} driver_index={selectedDriverIndex} handleImageHover={handleImageHover} handleImageClick={handleImageClick} />
       </div>
+
+      {/* <div className='bg-gradient-to-r from-indigo-100 via-purple-100 to-teal-100 h-screen dark:from-lime-200 dark:via-gray-600 dark:to-gray-600 pt-32'>
+        
+      </div> */}
     </motion.div>
   )
 }
