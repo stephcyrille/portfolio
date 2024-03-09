70988aa8dc0843d93d39b14c928e2c2dc91025c1
Stephcyrille <stephcyril.sc@gmail.com>
2024-03-09 11:59:27 +0100

    adding circuit data

---
+++ src/pages/f1-predictor/data/circuits.js
@@ -0,0 +1,268 @@
+import round1 from '../../../assets/images/predictor/races/r1.avif';
+import circuit1 from '../../../assets/images/predictor/races/cr1.avif';
+import round2 from '../../../assets/images/predictor/races/r2.avif';
+import circuit2 from '../../../assets/images/predictor/races/cr2.avif';
+import round3 from '../../../assets/images/predictor/races/r3.avif';
+import circuit3 from '../../../assets/images/predictor/races/cr3.avif';
+import round4 from '../../../assets/images/predictor/races/r4.avif';
+import circuit4 from '../../../assets/images/predictor/races/cr4.avif';
+import round5 from '../../../assets/images/predictor/races/r5.avif';
+import circuit5 from '../../../assets/images/predictor/races/cr5.avif';
+import round6 from '../../../assets/images/predictor/races/r6.avif';
+import circuit6 from '../../../assets/images/predictor/races/cr6.avif';
+import round7 from '../../../assets/images/predictor/races/r7.avif';
+import circuit7 from '../../../assets/images/predictor/races/cr7.avif';
+import round8 from '../../../assets/images/predictor/races/r8.avif';
+import circuit8 from '../../../assets/images/predictor/races/cr8.avif';
+import round9 from '../../../assets/images/predictor/races/r9.avif';
+import circuit9 from '../../../assets/images/predictor/races/cr9.avif';
+import round10 from '../../../assets/images/predictor/races/r10.avif';
+import circuit10 from '../../../assets/images/predictor/races/cr10.avif';
+import round11 from '../../../assets/images/predictor/races/r11.avif';
+import circuit11 from '../../../assets/images/predictor/races/cr11.avif';
+import round12 from '../../../assets/images/predictor/races/r12.avif';
+import circuit12 from '../../../assets/images/predictor/races/cr12.avif';
+import round13 from '../../../assets/images/predictor/races/r13.avif';
+import circuit13 from '../../../assets/images/predictor/races/cr13.avif';
+import round14 from '../../../assets/images/predictor/races/r14.avif';
+import circuit14 from '../../../assets/images/predictor/races/cr14.avif';
+import round15 from '../../../assets/images/predictor/races/r15.avif';
+import circuit15 from '../../../assets/images/predictor/races/cr15.avif';
+import round16 from '../../../assets/images/predictor/races/r16.avif';
+import circuit16 from '../../../assets/images/predictor/races/cr16.avif';
+import round17 from '../../../assets/images/predictor/races/r17.avif';
+import circuit17 from '../../../assets/images/predictor/races/cr17.avif';
+import round18 from '../../../assets/images/predictor/races/r18.avif';
+import circuit18 from '../../../assets/images/predictor/races/cr18.avif';
+import round19 from '../../../assets/images/predictor/races/r19.avif';
+import circuit19 from '../../../assets/images/predictor/races/cr19.avif';
+import round20 from '../../../assets/images/predictor/races/r20.avif';
+import circuit20 from '../../../assets/images/predictor/races/cr20.avif';
+import round21 from '../../../assets/images/predictor/races/r21.avif';
+import circuit21 from '../../../assets/images/predictor/races/cr21.avif';
+import round22 from '../../../assets/images/predictor/races/r22.avif';
+import circuit22 from '../../../assets/images/predictor/races/cr22.avif';
+import round23 from '../../../assets/images/predictor/races/r23.avif';
+import circuit23 from '../../../assets/images/predictor/races/cr23.avif';
+import round24 from '../../../assets/images/predictor/races/r24.avif';
+import circuit24 from '../../../assets/images/predictor/races/cr24.avif';
+
+
+export const races = [
+  {
+    id: 1,
+    name: 'Bahrain International Circuit',
+    country: 'Barhain',
+    city: 'Manama',
+    date: '2024-03-02',
+    imgSrc: round1,
+    circuitImgSrc: circuit1,
+  },
+  {
+    id: 2,
+    name: 'Jeddah Corniche Circuit',
+    country: 'Saudi Arabia',
+    city: 'Jeddah',
+    date: '2024-03-09',
+    imgSrc: round2,
+    circuitImgSrc: circuit2,
+  },
+  {
+    id: 3,
+    name: 'Melbourne Grand Prix Circuit',
+    country: 'Australia',
+    city: 'Melbourne',
+    date: '2024-03-24',
+    imgSrc: round3,
+    circuitImgSrc: circuit3,
+  },
+  {
+    id: 4,
+    name: 'Suzuka International Racing Course',
+    country: 'Japan',
+    city: 'Suzuka',
+    date: '2024-04-07',
+    imgSrc: round4,
+    circuitImgSrc: circuit4,
+  },
+  {
+    id: 5,
+    name: 'Shanghai International Circuit',
+    country: 'China',
+    city: 'Shanghai',
+    date: '2024-04-21',
+    imgSrc: round5,
+    circuitImgSrc: circuit5,
+  },
+  {
+    id: 6,
+    name: 'Miami International Autodrome',
+    country: 'USA',
+    city: 'Miami',
+    date: '2024-05-05',
+    imgSrc: round6,
+    circuitImgSrc: circuit6,
+  },
+  {
+    id: 7,
+    name: 'Autodromo Enzo e Dino Ferrari',
+    country: 'Italy',
+    city: 'Imola',
+    date: '2024-05-19',
+    imgSrc: round7,
+    circuitImgSrc: circuit7,
+  },
+  {
+    id: 8,
+    name: 'Circuit de Monaco',
+    country: 'France',
+    city: 'Monaco',
+    date: '2024-05-26',
+    imgSrc: round8,
+    circuitImgSrc: circuit8,
+  },
+  {
+    id: 9,
+    name: 'Circuit Gilles-Villeneuve',
+    country: 'Canada',
+    city: 'Montréal',
+    date: '2024-06-09',
+    imgSrc: round9,
+    circuitImgSrc: circuit9,
+  },
+  {
+    id: 10,
+    name: 'Circuit de Barcelona-Catalunya',
+    country: 'Spain',
+    city: 'Barcelona',
+    date: '2024-06-23',
+    imgSrc: round10,
+    circuitImgSrc: circuit10,
+  },
+  {
+    id: 11,
+    name: 'Red Bull Ring',
+    country: 'Austria',
+    city: 'Spielberg',
+    date: '2024-06-30',
+    imgSrc: round11,
+    circuitImgSrc: circuit11,
+  },
+  {
+    id: 12,
+    name: 'Silverstone Circuit',
+    country: 'Great Britain',
+    city: 'Silverstone',
+    date: '2024-07-07',
+    imgSrc: round12,
+    circuitImgSrc: circuit12,
+  },
+  {
+    id: 13,
+    name: 'Hungaroring',
+    country: 'Hungary',
+    city: 'Mogyoród',
+    date: '2024-07-21',
+    imgSrc: round13,
+    circuitImgSrc: circuit13,
+  },
+  {
+    id: 14,
+    name: 'Circuit de Spa-Francorchamps',
+    country: 'Belgium',
+    city: 'Malmedi',
+    date: '2024-07-28',
+    imgSrc: round14,
+    circuitImgSrc: circuit14,
+  },
+  {
+    id: 15,
+    name: 'Circuit Zandvoort',
+    country: 'Netherlands',
+    city: 'Zandvoort',
+    date: '2024-08-25',
+    imgSrc: round15,
+    circuitImgSrc: circuit15,
+  },
+  {
+    id: 16,
+    name: 'Autodromo Nazionale Monza',
+    country: 'Italy',
+    city: 'Monza',
+    date: '2024-09-01',
+    imgSrc: round16,
+    circuitImgSrc: circuit16,
+  },
+  {
+    id: 17,
+    name: 'Baku City Circuit',
+    country: 'Azerbaijan',
+    city: 'Baku',
+    date: '2024-09-15',
+    imgSrc: round17,
+    circuitImgSrc: circuit17,
+  },
+  {
+    id: 18,
+    name: 'Marina Bay Street Circuit',
+    country: 'Singapore',
+    city: 'Marina Bay Street',
+    date: '2024-09-22',
+    imgSrc: round18,
+    circuitImgSrc: circuit18,
+  },
+  {
+    id: 19,
+    name: 'Circuit of The Americas',
+    country: 'USA',
+    city: 'Austin',
+    date: '2024-10-20',
+    imgSrc: round19,
+    circuitImgSrc: circuit19,
+  },
+  {
+    id: 20,
+    name: 'Autódromo Hermanos Rodríguez',
+    country: 'Mexico',
+    city: 'Mexico City',
+    date: '2024-10-27',
+    imgSrc: round20,
+    circuitImgSrc: circuit20,
+  },
+  {
+    id: 21,
+    name: 'Autódromo José Carlos Pace',
+    country: 'Brazil',
+    city: 'Sao Polo',
+    date: '2024-11-03',
+    imgSrc: round21,
+    circuitImgSrc: circuit21,
+  },
+  {
+    id: 22,
+    name: 'Las Vegas Strip Circuit',
+    country: 'USA',
+    city: 'Las Vegas',
+    date: '2024-11-23',
+    imgSrc: round22,
+    circuitImgSrc: circuit22,
+  },
+  {
+    id: 23,
+    name: 'Lusail International Circuit',
+    country: 'Qatar',
+    city: 'Losail',
+    date: '2024-11-01',
+    imgSrc: round23,
+    circuitImgSrc: circuit23,
+  },
+  {
+    id: 24,
+    name: 'Yas Marina Circuit',
+    country: 'UAE',
+    city: 'Abu Dhabi',
+    date: '2024-12-08',
+    imgSrc: round24,
+    circuitImgSrc: circuit24,
+  },
+]
