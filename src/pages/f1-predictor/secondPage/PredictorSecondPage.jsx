39f16638d6654889c5521631a23f08e1f81cde8c
Stephcyrille <stephcyril.sc@gmail.com>
2024-03-18 19:08:13 +0100

    add flags static and fix responsive on the second step

--- src/pages/f1-predictor/secondPage/PredictorSecondPage.jsx
+++ src/pages/f1-predictor/secondPage/PredictorSecondPage.jsx
@@ -71,11 +71,11 @@ export const PredictorSecondPage = () => {
       <div className="relative mix-blend-overlay">
         <img src={circuitList.length > 0 ? circuitList[selectedDriverIndex].card_img : ''} alt="BannerImage" className="absolute h-screen lg:h-screen w-full object-cover object-right z-10" />
         <div className="absolute z-20 bg-gradient-to-r from-gray-900 via-gray-600 to-red-300 dark:from-gray-900 dark:via-lime-800 dark:to-gray-900 h-screen lg:h-screen w-full opacity-60" />
-        <div className="absolute z-20 h-screen lg:h-screen w-full pt-24 lg:pt-32 md:pt-32">
+        <div className="absolute z-20 h-screen lg:h-screen w-full pt-24 lg:pt-24 md:pt-24">
         <button
           onClick={handleBackClick}
           className="bg-transparent text-red-100 dark:border-lime-600 dark:hover:bg-lime-600 hover:text-white hover:bg-red-600 dark:hover:text-blue-100 dark:text-lime-300 py-2 px-4 rounded-full 
-          focus:outline-none transition duration-200 mb-4 lg:mb-0 md:mb-0 hover:shadow-xl lg:ml-12 hover:-translate-x-4"
+          focus:outline-none transition duration-200 mb-0 hover:shadow-xl lg:ml-12 hover:-translate-x-4"
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline mr-2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
@@ -84,20 +84,20 @@ export const PredictorSecondPage = () => {
           Select the driver
         </button>
 
-        <div className='text-amber-100 dark:text-lime-400 pb-2 md:pb-6 lg:pb-6 bg-amber-800 dark:bg-amber-600 bg-opacity-20 dark:bg-opacity-20 py-4'>
+        <div className='text-amber-100 dark:text-lime-400 pb-2 h-24 lg:h-16 md:pb-6 lg:pb-6 bg-amber-800 dark:bg-amber-600 bg-opacity-20 dark:bg-opacity-20 my-4'>
           <ItemNameDisplay data={circuitList} item_index={selectedDriverIndex} textSize="text-5xl" textSizeSm='text-2xl' race={true} />
         </div>
         <div className="mx-auto lg:flex md:flex max-w-7xl items-center justify-between px-6 lg:px-8">
-          <div className="md:py-8 sm:p-8 lg:grid lg:grid-cols-2 lg:static lg:max-h-full bg-white lg:overflow-visible rounded-lg shadow-lg dark:bg-gray-700 bg-opacity-80 dark:bg-opacity-70">
-            <img className="absolute w-96 z-10 opacity-15" alt='Flag' src="https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53E6/2F4C/EE37/B2B6/6292/0A28/100B/04E0/Flag_of_Bahrain_Fotor.jpg" />
+          <div className="md:py-4 sm:p-8 lg:grid lg:grid-cols-2 lg:static lg:max-h-full bg-white lg:overflow-visible rounded-lg shadow-lg dark:bg-gray-700 bg-opacity-80 dark:bg-opacity-70">
+            <img className="absolute w-96 z-10 opacity-15" alt='Flag' src={circuitList.length > 0 ? circuitList[selectedDriverIndex].country_img : ''} />
             <div className="p-6 flex flex-col z-50 text-amber-950 dark:text-amber-200 text-xl lg:text-2xl md:text-2xl justify-evenly">
                 {/* TODO Add the round number and the date of the race */}
-                <h3><span className='underline font-bold'>Round {circuitList[selectedDriverIndex].id}:</span> <span className="text-sm lg:text-xl md:text-xl">{circuitList[selectedDriverIndex].name}</span></h3>
-                <h3><span className='underline font-bold'>Location:</span> <span className="text-sm lg:text-xl md:text-xl">{circuitList[selectedDriverIndex].location}, {circuitList[selectedDriverIndex].country}</span></h3>
-                <h3><span className='underline font-bold'>Date</span>: <span className="text-sm lg:text-xl md:text-xl">{circuitList[selectedDriverIndex].date}</span></h3>
+                <h3><span className='underline font-bold'>Round {circuitList.length > 0 && circuitList[selectedDriverIndex].id}:</span> <span className="text-sm lg:text-xl md:text-xl">{circuitList.length > 0 && circuitList[selectedDriverIndex].name}</span></h3>
+                <h3><span className='underline font-bold'>Location:</span> <span className="text-sm lg:text-xl md:text-xl">{circuitList.length > 0 &&  circuitList[selectedDriverIndex].location}, {circuitList.length > 0 && circuitList[selectedDriverIndex].country}</span></h3>
+                <h3><span className='underline font-bold'>Date</span>: <span className="text-sm lg:text-xl md:text-xl">{circuitList.length > 0 &&circuitList[selectedDriverIndex].date}</span></h3>
             </div>
-            <div className="p-6 md:flex lg:flex md:justify-center lg:justify-center">
-              <img className='h-60 md:h-80 lg:h-80 self-center' src={circuitList.length > 0 ? circuitList[selectedDriverIndex].circuit_img : ''} alt="Stephane Cyrille" />
+            <div className="p-2 lg:p-6 md:flex lg:flex md:justify-center lg:justify-center">
+              <img className='h-48 md:h-80 lg:h-80 self-center' src={circuitList.length > 0 ? circuitList[selectedDriverIndex].circuit_img : ''} alt="Stephane Cyrille" />
             </div>
           </div>
         </div>
